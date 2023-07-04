e6dd12e97a034c13c43842747f43b917bf2f6193
Yachiyo <Yachiyo448@gmail.com>
2023-07-04 14:30:11 +0900

    課題1.32更新

--- profile/src/profile2/Main.java
+++ profile/src/profile2/Main.java
@@ -3,13 +3,17 @@ package profile2;
 class Main {
 	public static void main(String[] argos) {
 		Person person1 = new Person("鈴木太郎", 20, 1.7, 60);
+		// 名前と年を出力
 		person1.print();
 
 		Person person2 = new Person("山田花子", 22, 1.5, 40);
+		// 名前と年を出力
 		person2.print();
 
+		// 合計人数をメッセージ出力
 		System.out.println("合計" + Person.count + "人です");
 
+		// 合計人数をメッセージ出力
 		person1.printCount();
 	}
 }
--- profile/src/profile2/Person.java
+++ profile/src/profile2/Person.java
@@ -1,30 +1,36 @@
 package profile2;
 
 class Person {
+	// フィールドの定義
 	public String name;
 	public int age;
 	public double height;
 	public double weight;
 	public static int count = 0;
 
+	// コンストラクタの定義
 	Person(String name, int age, double height, double weight) {
 		this.name = name;
 		this.age = age;
 		this.height = height;
 		this.weight = weight;
 
+		// 人数の値を1増やす
 		Person.count++;
 	}
 
+	// BMIをメッセージ出力
 	public double bmi() {
 		return this.weight / this.height / this.height;
 	}
 
+	// 名前と年をメッセージ出力
 	public void print() {
 		System.out.println("名前は" + this.name + "です");
 		System.out.println("年は" + this.age + "です");
 	}
 
+	// 合計人数をメッセージ出力
 	public void printCount() {
 		System.out.println("合計" + Person.count + "人です");
 	}
